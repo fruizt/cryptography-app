1ab0c75b707277103618b4e301f8411ec7e15fce
Fabian Ruiz <40047900+F4B123@users.noreply.github.com>
2022-09-08 21:26:01 -0500

    shift

--- other/my-app/src/App.js
+++ other/my-app/src/App.js
@@ -1,11 +1,14 @@
 import {  Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Shift from "./pages/Shift";
 
 function App() {
   return (
 
       <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/classic" element={<Home />} />
+          <Route path="/shift" element={<Shift />} />
       </Routes>
 
   );
