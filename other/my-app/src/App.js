3228c94d6c4e4d2c32673ec21d268348272dd2a6
dnunezqq <dnunezquintero@gmail.com>
2022-09-13 02:32:40 -0500

    last

--- other/my-app/src/App.js
+++ other/my-app/src/App.js
@@ -16,12 +16,12 @@ function App() {
         <div className="container"><NavLink className="navbar-brand d-flex align-items-center" to="/"><span className="bs-icon-sm bs-icon-circle bs-icon-primary shadow d-flex justify-content-center align-items-center me-2 bs-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" className="bi bi-bezier">
                         <path fillRule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
                         <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
-                    </svg></span><span>Krypsus</span></NavLink><button data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navcol-1"><span className="visually-hidden">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
+                    </svg></span><span>Secrypt</span></NavLink><button data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navcol-1"><span className="visually-hidden">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navcol-1">
                 <ul className="navbar-nav mx-auto">
                     <li className="nav-item"><NavLink className="nav-link" to="/">Home</NavLink></li>
                     <li className="nav-item">
-                        <div className="nav-item dropdown nav-link active"><NavLink className="dropdown-toggle" aria-expanded="false" data-bs-toggle="dropdown" to="/">classNameic&nbsp;</NavLink>
+                        <div className="nav-item dropdown nav-link active"><NavLink className="dropdown-toggle" aria-expanded="false" data-bs-toggle="dropdown" to="/">Classic Methods&nbsp;</NavLink>
                             <div className="dropdown-menu dropdown-menu-dark"><NavLink className="dropdown-item" to="/shift">Shift</NavLink><NavLink className="dropdown-item" to="/substitution">Substitution</NavLink><NavLink className="dropdown-item" to="/affine">Affine</NavLink><NavLink className="dropdown-item" to="/vigenere">Vigenere</NavLink><NavLink className="dropdown-item" to="/hill">Hill</NavLink><NavLink className="dropdown-item" to="/permutation">Permutation</NavLink></div>
                         </div>
                     </li>
@@ -44,42 +44,10 @@ function App() {
       </Routes>
       <footer className="bg-dark">
         <div className="container py-4 py-lg-5">
-            <div className="row justify-content-center">
-                <div className="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column item">
-                    <h3 className="fs-6 fw-bold">Services</h3>
-                    <ul className="list-unstyled">
-                        <li><a href="#">Web design</a></li>
-                        <li><a href="#">Development</a></li>
-                        <li><a href="#">Hosting</a></li>
-                    </ul>
-                </div>
-                <div className="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column item">
-                    <h3 className="fs-6 fw-bold">About</h3>
-                    <ul className="list-unstyled">
-                        <li><a href="#">Company</a></li>
-                        <li><a href="#">Team</a></li>
-                        <li><a href="#">Legacy</a></li>
-                    </ul>
-                </div>
-                <div className="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column item">
-                    <h3 className="fs-6 fw-bold">Careers</h3>
-                    <ul className="list-unstyled">
-                        <li><a href="#">Job openings</a></li>
-                        <li><a href="#">Employee success</a></li>
-                        <li><a href="#">Benefits</a></li>
-                    </ul>
-                </div>
-                <div className="col-lg-3 text-center text-lg-start d-flex flex-column align-items-center order-first align-items-lg-start order-lg-last item social">
-                    <div className="fw-bold d-flex align-items-center mb-2"><span className="bs-icon-sm bs-icon-circle bs-icon-primary d-flex justify-content-center align-items-center bs-icon me-2"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" className="bi bi-bezier">
-                                <path fillRule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
-                                <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
-                            </svg></span><span>Krypsus</span></div>
-                    <p className="text-muted copyright">Sem eleifend donec molestie, integer quisque orci aliquam.</p>
-                </div>
-            </div>
+            
             <hr/>
             <div className="text-muted d-flex justify-content-between align-items-center pt-3">
-                <p className="mb-0">Copyright © 2022 Brand</p>
+                <p className="mb-0">Copyright © 2022 Secrypt</p>
                 <ul className="list-inline mb-0">
                     <li className="list-inline-item"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" className="bi bi-facebook">
                             <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
--- other/my-app/src/pages/Home.js
+++ other/my-app/src/pages/Home.js
@@ -1,23 +1,24 @@
 
 
 const Home = () => {
+    const slogan ="Security is not just computer science – it’s a mindset"
   return (
     <div>
-        
+        {/* <NavLink className="nav-link" to="/">Home</NavLink> */}
     <header className="bg-dark">
         <div className="container pt-4 pt-xl-5">
             <div className="row pt-5">
                 <div className="col-md-8 col-xl-6 text-center text-md-start mx-auto">
                     <div className="text-center">
-                        <p className="fw-bold text-success mb-2">Voted #1 Worldwide</p>
-                        <h1 className="fw-bold">The best solution for you and your customers</h1>
+                        {/* <p className="fw-bold text-success mb-2">Voted #1 Worldwide</p> */}
+                        <h1 className="fw-bold">{slogan}</h1>
                     </div>
                 </div>
                 <div className="col-12 col-lg-10 mx-auto">
-                    <div className="position-relative" style={{display: "flex",flexWrap: "wrap",justifyContent: "flex-end"}}>
-                        <div style={{position: "relative",flex: "0 0 45%",transform: "translate3d(-15%, 35%, 0)"}}><img className="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.8" src="assets/img/products/3.jpg"/></div>
-                        <div style={{position: "relative",flex: "0 0 45%",transform: "translate3d(-5%, 20%, 0)"}}><img className="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.4" src="assets/img/products/2.jpg"/></div>
-                        <div style={{position: "relative",flex: "0 0 60%",transform: "translate3d(0, 0%, 0)"}}><img className="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.25" src="assets/img/products/1.jpg"/></div>
+                    <div className="position-relative" style={{display: "flex",flexWrap: "wrap",justifyContent: "center",alignContent:"center"}}>
+                        <div style={{position: "relative",flex: "0 0 45%",alignContent:"center",justifyContent:"center"}}><img className="img-fluid" style={{alignContent:"center"}}data-bss-parallax="" data-bss-parallax-speed="0.8" src="assets/img/products/3.jpg"/></div>
+                        {/* <div style={{position: "relative",flex: "0 0 45%",transform: "translate3d(-5%, 20%, 0)"}}><img className="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.4" src="assets/img/products/2.jpg"/></div> */}
+                        {/* <div style={{position: "relative",flex: "0 0 60%",transform: "translate3d(0, 0%, 0)"}}><img className="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.25" src="assets/img/products/1.jpg"/></div> */}
                     </div>
                 </div>
             </div>
@@ -25,7 +26,7 @@ const Home = () => {
     </header>
     <section className="py-5">
         <div className="container text-center py-5">
-            <p className="mb-4" style={{fontSize: "1.6rem"}}>Used by <span className="text-success"><strong>2400+</strong></span>&nbsp;of the best companies in the world.</p><a href="#"> <img className="m-3" src="assets/img/brands/google.png"/></a><a href="#"> <img className="m-3" src="assets/img/brands/microsoft.png"/></a><a href="#"> <img className="m-3" src="assets/img/brands/apple.png"/></a><a href="#"> <img className="m-3" src="assets/img/brands/facebook.png"/></a><a href="#"> <img className="m-3" src="assets/img/brands/twitter.png"/></a>
+            <p className="mb-4" style={{fontSize: "1.6rem"}}>Don´t be shy try our app</p>
         </div>
     </section>
     <section>
@@ -45,7 +46,7 @@ const Home = () => {
                                         <path fillRule="evenodd" d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z"></path>
                                     </svg></div>
                                 <h5 className="fw-bold card-title">Shift Encryption</h5>
-                                <p className="text-muted card-text mb-4">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p><a className="btn btn-primary shadow" role="button" href="shift.html">Try</a>
+                                <p className="text-muted card-text mb-4">The encryption step performed by a Caesar cipher is often incorporated as part of more complex schemes, such as the Vigenère cipher, and still has modern application in the ROT13 system.</p><a className="btn btn-primary shadow" role="button" href="shift.html">Try</a>
                             </div>
                         </div>
                     </div>
@@ -56,7 +57,7 @@ const Home = () => {
                                         <path fillRule="evenodd" d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z"></path>
                                     </svg></div>
                                 <h5 className="fw-bold card-title">Substitution Encryption</h5>
-                                <p className="text-muted card-text mb-4">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p><a className="btn btn-primary shadow" role="button" href="substitution.html">Try</a>
+                                <p className="text-muted card-text mb-4">Substitution ciphers can be compared with transposition ciphers. In a transposition cipher, the units of the plaintext are rearranged in a different and usually quite complex order, but the units themselves are left unchanged.</p><a className="btn btn-primary shadow" role="button" href="substitution.html">Try</a>
                             </div>
                         </div>
                     </div>
@@ -67,7 +68,7 @@ const Home = () => {
                                         <path fillRule="evenodd" d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z"></path>
                                     </svg></div>
                                 <h5 className="fw-bold card-title">Affine Encryption</h5>
-                                <p className="text-muted card-text mb-4">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p><a className="btn btn-primary shadow" role="button" href="affine.html">Try</a>
+                                <p className="text-muted card-text mb-4">The affine cipher is a type of monoalphabetic substitution cipher, where each letter in an alphabet is mapped to its numeric equivalent, encrypted using a simple mathematical function, and converted back to a letter.</p><a className="btn btn-primary shadow" role="button" href="affine.html">Try</a>
                             </div>
                         </div>
                     </div>
@@ -78,7 +79,7 @@ const Home = () => {
                                         <path fillRule="evenodd" d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z"></path>
                                     </svg></div>
                                 <h5 className="fw-bold card-title">Vigenere Encryption</h5>
-                                <p className="text-muted card-text mb-4">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p><a className="btn btn-primary shadow" role="button" href="vigener.html">Try</a>
+                                <p className="text-muted card-text mb-4">First described by Giovan Battista Bellaso in 1553, the cipher is easy to understand and implement, but it resisted all attempts to break it until 1863, three centuries later.</p><a className="btn btn-primary shadow" role="button" href="vigener.html">Try</a>
                             </div>
                         </div>
                     </div>
@@ -89,7 +90,7 @@ const Home = () => {
                                         <path fillRule="evenodd" d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z"></path>
                                     </svg></div>
                                 <h5 className="fw-bold card-title">Hill Encryption</h5>
-                                <p className="text-muted card-text mb-4">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p><a className="btn btn-primary shadow" role="button" href="hill.html">Try</a>
+                                <p className="text-muted card-text mb-4">In classical cryptography, the Hill cipher is a polygraphic substitution cipher based on linear algebra. Invented by Lester S. Hill in 1929, it was the first polygraphic cipher in which it was practical (though barely) to operate on more than three symbols at once.</p><a className="btn btn-primary shadow" role="button" href="hill.html">Try</a>
                             </div>
                         </div>
                     </div>
@@ -100,7 +101,7 @@ const Home = () => {
                                         <path fillRule="evenodd" d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z"></path>
                                     </svg></div>
                                 <h5 className="fw-bold card-title">Permutation Encryption</h5>
-                                <p className="text-muted card-text mb-4">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p><a className="btn btn-primary shadow" role="button" href="permutation.html">Try</a>
+                                <p className="text-muted card-text mb-4">To apply a cipher, a random permutation of size e is generated (the larger the value of e the more secure the cipher). The plaintext is then broken into segments of size e and the letters within that segment are permuted according to this key.</p><a className="btn btn-primary shadow" role="button" href="permutation.html">Try</a>
                             </div>
                         </div>
                     </div>
