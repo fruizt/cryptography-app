a53d094318da0abdef2e6ea8e9f931daa9d714ef
DavidCMeneses <dcasallasm@unal.edu.co>
2022-11-17 11:13:30 -0500

    Create rsaSignature.py
    Finalmente

---
+++ back_py/app/controller/signature_controller/rsaSignature.py
@@ -0,0 +1,122 @@
+from random import randint
+from Crypto.PublicKey import RSA
+from Crypto.Signature import DSS
+from Crypto.Hash import SHA256
+import base64
+
+class RsaSignature():
+    def __init__(self):
+        #generate initial key
+        self.key=RSA.generate(2048)
+        
+        #self.alpha=self.key.pointQ
+        #self.public=self.key.public_key()
+        #self.a=self.key.d
+        #self.beta=self.a*self.alpha
+        self.public=self.key.public_key()
+
+        self.n = self.key.n
+        self.e = self.key.e
+        self.d = self.key.d
+        self.p = self.key.p
+        self.q = self.key.q
+        
+        f = open('myprivatekey.pem','wt')
+        f.write(self.key.export_key(format='PEM'))
+        f.close()
+        f = open('mypublickey.pem','wt')
+        f.write(self.key.public_key().export_key(format='PEM'))
+        f.close()
+    
+    #method to generate new keys
+    def generateKeyRsa(self):
+        self.key=RSA.generate(2048)
+        self.public=self.key.public_key()
+        f = open('myprivatekey.pem','wt')
+        f.write(self.key.export_key(format='PEM'))
+        f.close()
+        f = open('mypublickey.pem','wt')
+        f.write(self.key.public_key().export_key(format='PEM'))
+        f.close()
+
+        self.n = self.key.n
+        self.e = self.key.e
+        self.d = self.key.d
+        self.p = self.key.p
+        self.q = self.key.q
+
+        result={"public":{
+                    "n":self.n._value,
+                    "e":self.e._value
+                        },
+                "private":{
+                    "d":self.d._value,
+                    "p":self.p._value,
+                    "q":self.q._value   
+                        }
+                }
+        return result
+
+    def setPublicKey(self,keyPublic):
+        self.public = RSA.import_key(keyPublic)
+        f = open('mypublickey.pem','wt')
+        f.write(self.public.export_key(format='PEM'))
+        f.close()
+        return "succes"
+
+    def setPrivateKey(self,keyPrivate):
+        self.key = RSA.import_key(keyPrivate)
+        
+        f = open('myprivatekey.pem','wt')
+        f.write(self.key.export_key(format='PEM'))
+        f.close()
+        f = open('mypublickey.pem','wt')
+        f.write(self.key.public_key().export_key(format='PEM'))
+        f.close()
+        return "succes"
+    #method to encrypt
+    def signFile(self,f):
+        
+        BLOCK_SIZE = 65536 # The size of each read from the file
+
+        file_hash = SHA256.new() # Create the hash object, can use something other than `.sha256()` if you wish
+        
+        fb = f.read(BLOCK_SIZE) # Read from the file. Take in the amount declared above
+        while len(fb) > 0: # While there is still data being read from the file
+            file_hash.update(fb) # Update the hash
+            fb = f.read(BLOCK_SIZE) # Read the next block from the file
+
+        #create key optional_____________
+        #key=ECC.construct(curve="NIST P-256",d=self.a,point_x=self.alpha.x,point_y=self.alpha.y)
+        
+        signer = DSS.new(self.key, 'fips-186-3')
+        signature = signer.sign(file_hash)
+
+        sign_result=(base64.b64encode(signature)).decode("utf-8")
+
+        return sign_result
+    def verifyFile(self,signature,f):
+        
+        BLOCK_SIZE = 65536 # The size of each read from the file
+
+        file_hash = SHA256.new() # Create the hash object, can use something other than `.sha256()` if you wish
+        
+        fb = f.read(BLOCK_SIZE) # Read from the file. Take in the amount declared above
+        while len(fb) > 0: # While there is still data being read from the file
+            file_hash.update(fb) # Update the hash
+            fb = f.read(BLOCK_SIZE)
+        
+        #decode signature
+        signature=base64.b64decode(signature)
+
+        #construct public key ------optional
+        #public_key=ECC.construct(curve="NIST P-256",point_x=self.alpha.x,point_y=self.alpha.y)
+        
+        #create object to verify
+        verifyer=DSS.new(self.public,'fips-186-3')
+
+        try:
+            verifyer.verify(file_hash,signature)
+            return "Valid"
+        except:
+            return "No valid"
